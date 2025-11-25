7364c700c1cd91bd64d5f51f7dd89dcb46e8b2e9
Egorchik2006 <egorm4546@gmail.com>
2025-11-25 21:14:20 +0300

    Задание 11

---
+++ individual.py
@@ -0,0 +1,7 @@
+number_1 = float(input("Enter a first number: "))
+number_2 = float(input("Enter second number: "))
+summ = number_1 + number_2
+diff = number_2 - number_1
+div = number_1 / number_2
+arithmetic_mean = summ/2
+print(f"{int(number_1)}+{int(number_2)}={int(summ)} {int(number_1)}-{int(number_2)}={int(diff)} {int(number_1)}/{int(number_2)}={div} ({int(number_1)}+{int(number_2)})/2={arithmetic_mean} ")
